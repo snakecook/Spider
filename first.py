5ec58b45f866a8f5771669f41f35cd673166bc6f
snakecook <761446814@qq.com>
2020-07-13 09:19:33 +0800

    first

---
+++ first.py
@@ -0,0 +1,9 @@
+import requests
+from requests import Session
+
+url = 'http://www.86pm25.com/city/kunshan.html'
+header = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+}
+
+session = Session()
